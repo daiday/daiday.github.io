f745c57d4118cfab4cc8389113ea35f3b2901986
daiyan@PC-3080 <daiday@163.com>
2023-11-14 17:06:11 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import {
+  Box,
+  Heading, IconButton, useColorMode,
+  Wrap,
+} from "@chakra-ui/react";
+import {MoonIcon, SunIcon} from "@chakra-ui/icons";
+
+import PreviewBox from "./components/PreviewBox.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function App() {
+  const {colorMode, toggleColorMode} = useColorMode();
+  const [dirs, setDirs] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    axios.get("/list.json").then(response => {
+      setDirs(response.data);
+    });
+    return undefined;
+  }, []);
+
+  return (
+    <>
+      <Box h={8} m={8}>
+        <Heading>A Very Simple Page</Heading>
+      </Box>
+
+      <Wrap justify={"center"} spacing={8}>
+        {dirs.map((value, index) => (
+          <PreviewBox key={index} folder={value}/>
+        ))}
+
+        {dirs.length > 2 && <Box opacity={0} w={"xs"} h={0} cursor={"default"} m={0} p={0}></Box>}
+        {dirs.length > 3 && <Box opacity={0} w={"xs"} h={0} cursor={"default"} m={0} p={0}></Box>}
+        {dirs.length > 4 && <Box opacity={0} w={"xs"} h={0} cursor={"default"} m={0} p={0}></Box>}
+
+      </Wrap>
+
+      <Box position={"fixed"} right={2} top={2}>
+        <IconButton onClick={toggleColorMode}
+                    aria-label={"ColorMode"}
+                    icon={colorMode === "light" ? <MoonIcon/> : <SunIcon/>}>
+        </IconButton>
+      </Box>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PreviewBox.tsx
@@ -0,0 +1,64 @@
+import {
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Skeleton,
+  SkeletonText,
+  Text
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+interface PreviewBoxProps {
+  folder: string;
+}
+
+interface PreviewData {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export default function PreviewBox({ folder }: PreviewBoxProps) {
+  const [previewData, setPreviewData] = useState<PreviewData | null>(null);
+  const [loaded, setLoaded] = useState(false);
+  const url = `./${folder}/index.html`;
+
+  useEffect(() => {
+    axios.get(url).then(response => {
+      response.data;
+      const doc = new DOMParser().parseFromString(response.data, "text/html");
+      const title = doc.querySelector("title")?.textContent || "";
+      const description = doc.querySelector("meta[name='description']")?.getAttribute("content") || "";
+      const imageName = doc.querySelector("meta[property='og:image']")?.getAttribute("content") || "";
+      const image = `./${folder}/${imageName}`;
+      setPreviewData({ title, description, image });
+      setLoaded(true);
+    });
+    return undefined;
+  }, [folder]);
+
+  return (
+    <LinkBox>
+      <Card w={"xs"}>
+        <CardBody>
+          <Skeleton isLoaded={loaded}>
+            <Image src={previewData?.image}></Image>
+          </Skeleton>
+          <LinkOverlay href={url}>
+            <Skeleton isLoaded={loaded}>
+              <Heading>{previewData?.title}</Heading>
+            </Skeleton>
+            <SkeletonText isLoaded={loaded}>
+              <Text textAlign={"left"}>{previewData?.description}</Text>
+            </SkeletonText>
+          </LinkOverlay>
+        </CardBody>
+      </Card>
+    </LinkBox>
+  );
+}
